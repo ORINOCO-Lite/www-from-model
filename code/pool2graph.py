2eade6b075fbcec59957a71d786d7f813f582281
Michael Hanke <michael.hanke@gmail.com>
2026-03-06 17:59:00 +0100

    feat: workflow to (re)build the navigation graph

---
+++ code/pool2graph.py
@@ -0,0 +1,100 @@
+import json
+from pprint import pprint
+import sys
+
+nodes = {}
+edges = {}
+
+wanted_node_types = {
+    "xyzri:XYZInstrument": 'instrument',
+    "xyzri:XYZOrganization": 'organization',
+    "xyzri:XYZPerson": 'person',
+    "xyzri:XYZProject": 'project',
+    "xyzri:XYZPublication": 'publication',
+}
+
+wanted_edge_types = {
+    'associated_with': 'associated_with',
+    'generated_by': 'generated_by',
+    'delegated_by': 'delegated_by',
+    'influenced_by': 'influenced_by',
+    'part_of': 'part_of',
+}
+
+
+def get_node_label(rec: dict) -> str | None:
+    for src in (
+        'short_name',
+        'name',
+        'title',
+        'family_name',
+    ):
+        label = rec.get(src)
+        if label:
+            if src == 'family_name':
+                return f'{rec["given_name"][0]}.{label}'
+            else:
+                return label
+    return None
+
+
+def get_node_url(rec: dict) -> str | None:
+    pid = rec['pid']
+    www_root_prefix = 'xyzrins:'
+    if pid.startswith(www_root_prefix):
+        return f'/{pid[len(www_root_prefix):]}'
+    return None
+
+
+def add_edge(src: str, target: str, kind: str) -> None:
+    edge_id = f'e{len(edges)}'
+    edges[edge_id] = {
+        'id': edge_id,
+        'source': src,
+        'target': target,
+        'type': kind,
+    }
+
+
+for line in sys.stdin:
+    rec = json.loads(line)
+    schema_type = rec.get('schema_type')
+    if schema_type not in wanted_node_types:
+        continue
+
+    pid = rec['pid']
+    for prop, edge_kind in wanted_edge_types.items():
+        for rel in rec.get(prop, []):
+            obj = rel['object'] if isinstance(rel, dict) else rel
+            add_edge(pid, obj, edge_kind)
+
+    nodes[pid] = {
+        'id': pid,
+        'label': get_node_label(rec),
+        'type': wanted_node_types[schema_type],
+        'size': 1,
+        'url': get_node_url(rec),
+    }
+
+kept_edges = []
+nodes_missing = set()
+for eid, edge in edges.items():
+    err = False
+    for prop in ('source', 'target'):
+        if edge[prop] not in nodes:
+            nodes_missing.add(edge[prop])
+            err = True
+    if err:
+        continue
+    nodes[edge['target']]['size'] += 1
+    kept_edges.append(edge)
+
+out = {'nodes': list(nodes.values()), 'edges': kept_edges}
+
+json.dump(out, sys.stdout)
+
+if nodes_missing:
+    pprint(
+        f"Missing nodes: {nodes_missing!r}",
+        stream=sys.stderr,
+    )
